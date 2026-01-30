f06bddee940207c173d846d2e9f00d3d63b473f0
Navmesh Srivastava <navmeshsrivastav815@gmail.com>
2026-01-30 21:24:59 +0530

    Day 12: Binary Search and Problems

---
+++ 10 Binary Search Problems/01_first-and-last-position-of-an-element-in-sorted-array.cpp
@@ -0,0 +1,58 @@
+// https://www.naukri.com/code360/problems/first-and-last-position-of-an-element-in-sorted-array_1082549?source=youtube&campaign=love_babbar_codestudio2&utm_source=youtube&utm_medium=affiliate&utm_campaign=love_babbar_codestudio2&leftPanelTabValue=SUBMISSION
+
+#include<vector>
+using namespace std;
+int firstOcc(vector<int>& arr, int n, int key) {
+
+    int s = 0, e = n-1;
+    int mid = s + (e-s)/2;
+    int ans = -1;
+    while(s<=e) {
+
+        if(arr[mid] == key){
+            ans = mid;
+            e = mid - 1;
+        }
+        else if(key > arr[mid]) {//Right me jao
+            s = mid + 1;
+        }
+        else if(key < arr[mid]) {//left me jao
+            e = mid - 1;
+        }
+
+        mid = s + (e-s)/2;
+    }
+    return ans;
+}
+
+int lastOcc(vector<int>& arr, int n, int key) {
+
+    int s = 0, e = n-1;
+    int mid = s + (e-s)/2;
+    int ans = -1;
+    while(s<=e) {
+
+        if(arr[mid] == key){
+            ans = mid;
+            s = mid + 1;
+        }
+        else if(key > arr[mid]) {
+            s = mid + 1;
+        }
+        else if(key < arr[mid]) {
+            e = mid - 1;
+        }
+
+        mid = s + (e-s)/2;
+    }
+    return ans;
+}
+
+pair<int, int> firstAndLastPosition(vector<int>& arr, int n, int k)
+{
+    pair<int,int> p;
+    p.first = firstOcc(arr, n, k);
+    p.second = lastOcc(arr, n, k);
+    
+    return p;
+}
